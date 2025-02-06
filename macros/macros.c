a78abbfef7412f52eab6030ef1536da76c7cfbf2
Hriday Hema Raj <112681613+Hriday04@users.noreply.github.com>
2025-02-06 14:13:01 -0800

    Update macros.c

--- macros/macros.c
+++ macros/macros.c
@@ -1,3 +1,13 @@
-#define	CHOICE(e,f,g) ((e)?(f):(g))
-#define MEDIAN(e,f,g) ((!!(e) + !!(f) + !!(g)) > 1)
-#define ROTATE(x,n) x >> n
+#ifndef MACROS_H
+#define MACROS_H
+
+/* Bitwise Choice Macro */
+#define CHOICE(e, f, g) ((e & f) ^ (~e & g))
+
+/* Bitwise Median Macro */
+#define MEDIAN(e, f, g) ((e & f) | (f & g) | (g & e))
+
+/* Right Rotate Macro */
+#define ROTATE(a, b) ((a >> b) | (a << (32 - b)))
+
+#endif /* MACROS_H */
